df54723650212d7d54a2d11e99271ba60fe9a089
thierry13790 <thierry.bousquet@orange.fr>
2022-02-21 20:09:53 +0100

    shrink not usefull files & examples from Cube

--- Projects/NUCLEO-WL55JC/Applications/SubGHz_Phy/SubGHz_Phy_PingPong/STM32CubeIDE/Debug/Utilities/stm32_tiny_sscanf.d
+++
@@ -1,5 +0,0 @@
-Utilities/stm32_tiny_sscanf.o: \
- D:/dev/Homekit_LetterBox/en.stm32cubewl_v1-1-0_v1.1.0/STM32Cube_FW_WL_V1.1.0/Utilities/misc/stm32_tiny_sscanf.c \
- D:/dev/Homekit_LetterBox/en.stm32cubewl_v1-1-0_v1.1.0/STM32Cube_FW_WL_V1.1.0/Utilities/misc/stm32_tiny_sscanf.h
-
-D:/dev/Homekit_LetterBox/en.stm32cubewl_v1-1-0_v1.1.0/STM32Cube_FW_WL_V1.1.0/Utilities/misc/stm32_tiny_sscanf.h:
